56d414445b909f0d740462aad1e7f00654339ba2
hamed18 <hamedcnec@gmail.com>
2024-09-03 13:35:36 +0600

    Load data based on the page number and size

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 
-// nTXex2fI0U4hgbhF
-// ema-john
-console.log(process.env.DB_USER, process.env.DB_PASS);
+//console.log(process.env.DB_USER, process.env.DB_PASS);
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3nkfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+//console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,7 +34,15 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
+        console.log('pagination query', req.query);
+
+        const result = await productCollection.find()
+        .skip(page*size)
+        .limit(size)
+        .toArray();
+        
         res.send(result);
     })
 
